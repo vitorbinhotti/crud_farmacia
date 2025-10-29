f197a998bd66119971846ab65c57d581079ba4c5
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-10-29 08:56:05 -0300

    fix: removendo pasta

--- autores/read_autor.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../banco-de-dados/db.php';
-$result = $conn->query("SELECT * FROM autores");
-
-while ($row = $result->fetch_assoc()) {
-    echo $row['id_autores']." - ".$row['nome']." - ".$row['nacionalidade']." - ".$row['ano_nascimento']."<br>";
-}
-?>
